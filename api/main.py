58dd444845ee3f52bc649ed4fef21424db01621e
Christian Bayquen <cgabayquen@gmail.com>
2022-12-19 10:09:02 +0800

    fix: fixed upload endpoint error 422

--- api/main.py
+++ api/main.py
@@ -2,15 +2,27 @@
 # See https://fastapi.tiangolo.com/tutorial/first-steps/
 
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import lib.PDFParser as PDFParser
 from io import BytesIO
 
 app = FastAPI()
 
+# set CORS policy to any origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # @app.post("/files/")
 # async def create_file(file: bytes = File()):
 #     file.save(f"./pdfs/{file.filename}")
 #     return {"data": "sheesh"}
+
+# accept pdf files and return parsed data
 
 @app.post("/upload/")
 async def create_upload_file(document: UploadFile = File(...)):
@@ -18,13 +30,17 @@ async def create_upload_file(document: UploadFile = File(...)):
     period = document.filename.rfind(".")
     file = BytesIO(await document.read())
     data = None
-    print(file)
+
     if document.filename[period:] == ".pdf":
         parser = PDFParser.PDFParser(file)
         data = parser.parsePDF()
     else:
         parser = PDFParser.PDFParser(file.read().decode("utf-8"))
-        # print(type(file.read()))
         print(parser.src)
         data = parser.parseText()
+
     return {"data": data}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
